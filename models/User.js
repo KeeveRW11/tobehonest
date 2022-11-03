7de39178d437645f2625472eb383c80675f4393d
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-02 22:14:24 -0400

    user model done. will review later to validate email

--- models/User.js
+++ models/User.js
@@ -1,6 +1,55 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema();
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // trim: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return  this.friends.reduce((total, friend) => total + friend.length + 1, 0)
+});
+
+thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }
+]
+
+friends: [
+    {type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+]
 
 const User = model('User', UserSchema);
 
