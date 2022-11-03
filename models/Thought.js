112f68517eff5e34ac18c7497472ccc40e550dd9
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-03 09:37:40 -0400

    update thought model and add date validator

--- models/Thought.js
+++ models/Thought.js
@@ -1,24 +1,68 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId(),
+
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },    
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: CreatedAtVal => dateFormat(CreatedAtVal)
+        }, 
+    },
+    {
+        toJson: {
+            // virtuals: true,
+            getters: true
+        }
+    }
+);
 
 const ThoughtSchema = new Schema (
     {
-        thoughtTest: {
+        thoughtText: {
             type: String,
             required: true,
-
+            minlength:1,
+            maxlength: 280 
         },
         createdAt: {
             type: Date,
             default: Date.now,
             get: CreatedAtVal => dateFormat(CreatedAtVal)
+        },
+        username: {
+            type: String,
+            required: true
         }
+        ,
+        reactions: [reactionSchema]
+        
     },
     {
         toJson: {
+            virtuals: true,
             getters: true
         }
     }
 );
+
+ThoughtSchema.virtual('reactionCount').get (function() {
+    return this.friends.reduce((total, friend) => total + friend.length + 1)
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
